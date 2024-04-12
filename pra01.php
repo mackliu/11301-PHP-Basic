a67038a66651cbbbaeaab60c03e4b52b903e9e20
mackliu <macklun@gmail.com>
2024-04-12 15:35:56 +0800

    閏年判斷

--- pra01.php
+++ pra01.php
@@ -97,5 +97,47 @@ echo "<span style='font-size:32px;color:$color'>";
 echo $words;
 echo "</span>";
 ?>
+
+<h2>閏年判斷，給定一個西元年份，判斷是否為閏年</h2>
+<style>
+    .desc{
+        width:500px;
+        padding:10px;
+        background: lightsalmon;
+        color:darkmagenta;
+        border: 1px solid #666;
+        box-shadow: 2px 2px 5px lightsalmon;
+    }
+</style>
+<div class="desc">地球對太陽的公轉一年的真實時間大約是365天5小時48分46秒，因此以365天定為一年 的狀況下，每年會多出近六小時的時間，所以每隔四年設置一個閏年來消除這多出來的一天。</div>
+<ul>
+    <li>公元年分除以4不可整除，為平年。</li>
+    <li>公元年分除以4可整除但除以100不可整除，為閏年。</li>
+    <li>公元年分除以100可整除但除以400不可整除，為平年。</li>
+</ul>
+<?php 
+$year=2024;
+
+echo "年份為".$year;
+echo "<br>";
+
+if($year%4 == 0 && $year%100!=0 || $year%400==0){
+    echo $year . "是閏年";
+
+}else{
+    echo $year . "是平年";
+}
+
+
+?>
+<p>&nbsp</p>
+<p>&nbsp</p>
+<p>&nbsp</p>
+<p>&nbsp</p>
+<p>&nbsp</p>
+<p>&nbsp</p>
+<p>&nbsp</p>
+<p>&nbsp</p>
+<p>&nbsp</p>
 </body>
 </html>
