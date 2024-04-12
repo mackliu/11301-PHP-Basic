08c83d0f43f57bfa2557958996ed7399be81a915
mackliu <macklun@gmail.com>
2024-04-12 10:38:29 +0800

    while 迴圈應用

--- loop.php
+++ loop.php
@@ -1,3 +1,4 @@
+<h2>FOR</h2>
 <?php
 //echo $i;
 for($i=0;$i<10;$i++){
@@ -7,3 +8,27 @@ for($i=0;$i<10;$i++){
 
 echo "<br>";
 echo $i;
+echo "<hr>";
+
+//echo $i;
+for($i=0;$i>-100;$i=$i*2-1){
+    echo $i*10;
+    echo "<br>";
+}
+
+echo "<br>";
+echo $i;
+
+?>
+<h2>WHILE</h2>
+<?php
+$score=10;
+echo "原始成績=".$score;
+while($score<60){
+    $score=$score+10;
+}
+
+echo "<br>";
+echo "調整結果=".$score;
+
+?>
