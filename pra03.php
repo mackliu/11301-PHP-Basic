aadc436fae7dba97a767164f85b67cb2773ba6b5
mackliu <macklun@gmail.com>
2024-04-15 15:24:51 +0800

    完成正三角形

--- pra03.php
+++ pra03.php
@@ -4,12 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>畫星星</title>
+    <style>
+        *{
+            font-family: 'Courier New', Courier, monospace;
+        }
+    </style>
 </head>
 <body>
 <h2>直角三角形</h2>    
 
 <?php
-for($i=0;$i<5;$i++){
+$stars=7;
+
+for($i=0;$i<$stars;$i++){
     for($j=0;$j<=$i;$j++){
         echo "*";
     }
@@ -17,6 +24,38 @@ for($i=0;$i<5;$i++){
 }
 
 ?>
+<h2>倒直角三角形</h2>    
 
+<?php
+for($i=$stars;$i>0;$i--){
+    for($j=0;$j<$i;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+
+?>
+<h2>正三角形</h2>    
+
+<?php
+for($i=0;$i<$stars;$i++){
+    for($k=0;$k<$stars-1-$i;$k++){
+        echo "&nbsp;";
+    }
+
+    for($j=0;$j<$i*2+1;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+
+?>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 </body>
 </html>
