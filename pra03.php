7422ce50f0cc716675c078a91b213e17675ae2d3
mackliu <macklun@gmail.com>
2024-04-15 14:18:15 +0800

    直角三角形

---
+++ pra03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>畫星星</title>
+</head>
+<body>
+<h2>直角三角形</h2>    
+
+<?php
+for($i=0;$i<5;$i++){
+    for($j=0;$j<=$i;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+
+?>
+
+</body>
+</html>
