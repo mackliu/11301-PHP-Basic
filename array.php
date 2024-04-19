4fc681ebccd892f79302743b3a23ec47cb86024a
mackliu <macklun@gmail.com>
2024-04-19 15:25:07 +0800

    威力彩號不重覆-使用while

--- array.php
+++ array.php
@@ -94,9 +94,28 @@ foreach($ninenine as $idx => $nine ){
 
 }
 echo "</table>";
-
+echo "陣列中有".count($ninenine)."個元素";
 ?>
+<h2>威力彩電腦選號沒有重覆號碼(利用while迴圈)</h2>
 
+<ul>
+    <li>使用亂數函式rand($a,$b)來產生號碼</li>
+    <li>將產生的號碼順序存入陣列中</li>
+    <li>每次存入陣列中時會先檢查陣列中的資料有沒有重覆</li>
+    <li>完成選號後將陣列內容印出</li>
+</ul>
+
+<?php
+$lotto=[];
+while(count($lotto)<6){
+    $tmp=rand(1,38);
+    if(!in_array($tmp,$lotto)){
+        $lotto[]=$tmp;
+    }
+}
+
+echo join(",",$lotto);
+?>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
