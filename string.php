1cdb256ab258a31d67ec1263c0a0aa39b461d2a1
mackliu <macklun@gmail.com>
2024-04-25 10:55:22 +0800

    字串整合應用

--- string.php
+++ string.php
@@ -71,10 +71,36 @@ $s='The reason why a great man is great is that he resolves to be a great man';
 $new=mb_substr($s,0,10);
 echo $new;
 echo "...";
+echo "<br>";
+//echo str_repeat(".",3);
+$s='中央氣象署地震報告顯示，今天凌晨至上午8時22分，共發生14起地震，其中台灣東部海域在凌晨2時11分發生規模5.6地震，不到一分鐘，又發生一起規模5.5地震，全台有感，許多民眾被嚇醒。';
+$new=mb_substr($s,8,20);
+echo $new;
+echo "...";
 //echo str_repeat(".",3);
 ?>
 
 <hr>
+<h2>尋找字串與HTML、css整合應用</h2>
+<ul>
+    <li>給定一個句子，將指定的關鍵字放大</li>
+    <li>“學會PHP網頁程式設計，薪水會加倍，工作會好找”</li>
+    <li>請將上句中的 “程式設計” 放大字型或變色.</li>
+</ul>
+
+<?php
+$key='網頁';
+$color='pink';
+$size='24px';
+$s="學會PHP網頁程式設計，薪水會加倍，工作會好找";
+$s=str_replace($key,"<span style='color:$color;font-size:$size'>$key</span>",$s);
+
+
+echo $s;
+?>
+
+
+
 
 
 <p>&nbsp;</p>
