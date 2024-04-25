254a71b98094e4ead24aafdaafae071215da2854
mackliu <macklun@gmail.com>
2024-04-25 16:19:23 +0800

    計算那些格子要放日期

--- calendar.php
+++ calendar.php
@@ -25,9 +25,22 @@
 </ul>
 
 <?php 
-$month=4;
+$month=9;
 echo "月份:".$month;
-
+echo "<br>";
+$firstDay=strtotime(date("Y-$month-1"));
+$firstWeekStartDay=date("w",$firstDay);
+echo "第一周的開始是第".$firstWeekStartDay."日";
+$days=date("t",$firstDay);
+$lastDay=strtotime(date("Y-$month-$days"));
+echo "<br>";
+echo "最後一天是".date("Y-m-d",$lastDay);
+/* 
+4  1  1 -0
+5  3  3 -2
+6  6  6 -5
+7  1  1 -0
+*/
 echo "<table>";
 echo "<tr>";
 echo "<td>日</td>";
@@ -41,10 +54,22 @@ echo "</tr>";
 for($i=0;$i<6;$i++){
     echo "<tr>";
     for($j=0;$j<7;$j++){
-        echo "<td>";
-
-
-        echo "</td>";
+        if($i==0 && $j>=$firstWeekStartDay ){
+            echo "<td>";
+            echo    $i*7+$j-($firstWeekStartDay-1);
+            echo "</td>";
+        }else if($i>0){
+            echo "<td>";
+            if($i*7+$j-($firstWeekStartDay-1)<=$days){
+                echo $i*7+$j-($firstWeekStartDay-1);
+            }else{
+                echo "&nbsp;";
+            }
+            echo "</td>";
+            
+        }else{
+            echo "<td></td>";
+        }
     }
     echo "</tr>";
 }
