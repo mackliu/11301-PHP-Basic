0dc5560f1d0a9aa52dbe56b74893b9d9ab7aa851
mackliu <macklun@gmail.com>
2024-04-25 10:26:56 +0800

    其他字串練習

--- string.php
+++ string.php
@@ -46,4 +46,41 @@ echo "<pre>";
 print_r($result);
 echo "</pre>";
 ?>
+
+<h2>字串組合</h2>
+<ul>
+    <li>
+        將上例陣列重新組合成“this is a book”
+    </li>
+</ul>
+
+<?php
+$result=join(' ',$result);
+echo $result;
+
+?>
+
+<h2>子字串取用</h2>
+<ul>
+    <li>
+        將” The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…”
+    </li>
+</ul>
+<?php
+$s='The reason why a great man is great is that he resolves to be a great man';
+$new=mb_substr($s,0,10);
+echo $new;
+echo "...";
+//echo str_repeat(".",3);
+?>
+
 <hr>
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
